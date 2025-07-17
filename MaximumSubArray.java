65e4b9c72370618cc354a3e4450a31beb2498a50
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-07-17 17:45:09 -0500

    Update MaximumSubArray.java

--- MaximumSubArray.java
+++ MaximumSubArray.java
@@ -1,45 +1,15 @@
-//  Time complexity - O(n)
+// // Time complexity - O(n)
 // Space complexity -O(1)
 //Solved on leetcode - yes
 //FAced any issues - NO
-// The algorithm finds the first index i from the end where nums[i] < nums[i + 1], then swaps it with the next larger element on its right. Finally, it reverses the subarray from i + 1 to the end to get the next lexicographically greater permutation.
+// Calculate the running sum considering the max of current element and sum of previous running sum and current element
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int i;
-        for (i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                break;
-            }
+    public int maxSubArray(int[] nums) {
+        int rSum=0,max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++) {
+            rSum = Math.max(rSum+nums[i], nums[i]);
+            max = Math.max(max, rSum);
         }
-
-        if (i < 0) {
-            reverse(nums, 0, nums.length - 1);
-        } else {
-            int a = getNextLargestIndex(nums, i);
-            int temp = nums[a];
-            nums[a] = nums[i];
-            nums[i] = temp;
-            reverse(nums, i + 1, nums.length - 1);
-        }
-    }
-
-    public void reverse(int nums[], int i, int j) {
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
-    }
-
-    public int getNextLargestIndex(int nums[], int i) {
-        int index = i + 1;
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[j] > nums[i] && nums[j] <= nums[index]) {
-                index = j;
-            }
-        }
-        return index;
+        return max;
     }
 }
